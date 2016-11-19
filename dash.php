f6a3bd4c4cb80d6f205e84f58c0c2bd60261a5eb
Yash <Yash>
2016-11-19 13:00:26 +0530

    Hello

--- dash.php
+++ dash.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="dash.css">
+	<link rel="stylesheet" type="text/css" href="css/dash.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
 	<script type="text/javascript" src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="sweetalert.css">
