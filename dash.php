ac0a54e8b7805b2819c671cba9aa77e90760ebd6
Yash <Yash>
2016-11-19 12:29:00 +0530

    Yo

---
+++ dash.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="dash.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+	<script type="text/javascript" src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="sweetalert.css">
+	<script type="text/javascript" src = "sweetalert.min.js"></script>
+</head>
+<body>
+<div class = "header">
+	<input id = "search"></div>
+</div>
+</body>
+</html>
