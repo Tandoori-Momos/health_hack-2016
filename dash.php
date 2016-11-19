1915f0bb1b544e126968e9eee1af3ef3d03a6805
Vishrut Malik <Vishrut Malik>
2016-11-19 17:20:35 +0530

    vishrut chutiya hain

--- dash.php
+++ dash.php
@@ -1,16 +1,32 @@
+<?php
+   session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>NextEv</title>
 	<link rel="stylesheet" type="text/css" href="css/dash.css">
+	<script type="text/javascript" src="jquery.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
-	<script type="text/javascript" src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="sweetalert.css">
-	<script type="text/javascript" src = "sweetalert.min.js"></script>
 </head>
 <body>
-<div class = "header">
-	<input id = "search"></div>
+	<div class = "header">
+		<div class="logo"><span style = "font-weight: 400;">EXUN</span>CLAN</div>
+		<div class="nav">
+			<div class = "nav-opt"><a href="index.html">Home</a></div>
+			<div class = "nav-opt"><a href = "login.html">Search</a></div>
+			<div class = "nav-opt"><a href="signup.html">Log Out</a></div>
+		</div>
+		</div>
+
+		<div class="ask">How early do you need an Organ?</div>
+		<div class="lin"></div>
+<div class="cont">
+		<div class="button">Very Urgently</div>
+		<div class="button no">In a Week's Time</div>
 </div>
+<div class="button2">I have Plenty of Time</div>
 </body>
 </html>
